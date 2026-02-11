2010eaa669ba3f0f36db870d4a8c33755656cd1d
Vuacheslav <soloomonslava1991@gmail.com>
2026-02-11 12:42:35 +0300

    .

--- dialogs/main_dialog.py
+++ dialogs/main_dialog.py
@@ -67,7 +67,7 @@ async def start_message_getter(dialog_manager: DialogManager, **kwargs):
 
 start_window = Window(
     Format('<b>{first_name} здравствуйте!</b>'
-          'На связи беспроводной умный дом HiTE PRO.\n\n'
+          ' На связи беспроводной умный дом HiTE PRO.\n\n'
           'Какая инструкция вас интересует?'),
     Column(
         Button(Const('Беспроводные радиовыключатели'), id ='1', on_click=switch),
